6328a66248b9254bf1540003e13f1ae36224c885
Regan <regancrowley1@gmail.com>
2023-05-26 15:02:20 +1000

    module 9

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-
-const template = `# Title
-
-## Description
-(Insert description here)
-
-## Installation
-(Insert Installations here)
-## Usage Information
-(Insert Usage information here)
-
-## License
-(Insert License here)
-
-## Contribution Guidelines
-(Insert guidelines here)
-
-## Test
-(Insert testing instructions here)
-
-## Questions
-(Insert username and email here)
-`
+const generateMarkdown = require('./utils/generateMarkdown')
 
 inquirer
   .prompt([
@@ -36,7 +14,7 @@ inquirer
       type: 'input',
       name: 'description',
       message: 'What is the project description?',
-     
+
     },
     {
       type: 'input',
@@ -44,43 +22,41 @@ inquirer
       message: 'what installation instructions',
     },
     {
-        type: 'input',
-        name: 'usage',
-        message: 'what is the usage information',
-      },
-      {
-        type: 'list',
-        name: 'license',
-        message: 'what license are you using?',
-        choices: ['none', 'Apache License 2.0', 'MIT License', 'GNU General Public License v3.0'],
-      },
-      {
-        type: 'input',
-        name: 'contribution',
-        message: 'what is the contribution guide line?',
-      },
-      {
-        type: 'input',
-        name: 'tests',
-        message: 'what are the testing stats?',
-      },
-      {
-        type: 'input',
-        name: 'username',
-        message: 'what is your github username?',
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: 'what is your email??',
-      },
-
+      type: 'input',
+      name: 'usage',
+      message: 'what is the usage information',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'what license are you using?',
+      choices: ['none', 'Apache License 2.0', 'MIT License', 'GNU General Public License v3.0'],
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'what is the contribution guide line?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'what are the testing stats?',
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'what is your github username?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'what is your email?',
+    },
   ])
-  .then((dogs) => {
-    console.table(dogs)
-    const filename = `${dogs.name.toLowerCase().split(' ').join('')}.json`;
-
-    fs.writeFile(filename, JSON.stringify(dogs, null, '\t'), (err) =>
+  .then((promptData) => {
+    const filename = `${promptData.project.toLowerCase().split(' ').join('')}.md`;
+    const markdown = generateMarkdown(promptData)
+    fs.writeFile(filename, markdown, (err) =>
       err ? console.log(err) : console.log('Success!')
     );
   });
---
+++ project101.md
@@ -0,0 +1,31 @@
+# Project101
+
+## Description
+
+THis is a starter project for a readme
+
+## Installation
+
+Install Node
+
+## Usage Information
+
+It uses Node
+
+## License
+
+GNU General Public License v3.0
+
+## Contribution Guidelines
+
+Readme
+
+## Test
+
+Very good
+
+## Questions
+
+- regancr
+
+- Email.Email.com
